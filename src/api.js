e018615d857d0fb8d9299ac6b0c5781f326ae423
hannesdonel <mail@hannesdonel.de>
2021-09-29 09:40:37 +0200

    Make App available offline.

--- src/api.js
+++ src/api.js
@@ -71,6 +71,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
+
   const token = await getAccessToken();
   removeQuery();
   const url = `${EVENTS_ENDPOINT}/${token}`;
